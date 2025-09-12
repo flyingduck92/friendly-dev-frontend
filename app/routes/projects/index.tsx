f4c41ef04731944061be64e40bae5c4bd132741f
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-09-12 13:57:54 +0700

    Create loader to fetch projects

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,4 +1,16 @@
-const Projects = () => {
+import type { Route } from './+types/index'
+
+export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+  const res = await fetch('http://localhost:3000/projects')
+  const data = await res.json()
+
+  return { projects: data }
+}
+
+const Projects = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData
+  console.log(projects)
+
   return (
     <>
       <h2 className="text-3xl font-bold text-white mb-8">
