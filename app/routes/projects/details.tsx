e23b244e6afb8d867f9f68ba031f85e0c0cd3cdf
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-09-13 03:13:51 +0700

    Display a single project based on project id

--- app/routes/projects/details.tsx
+++ app/routes/projects/details.tsx
@@ -1,5 +1,7 @@
 import type { Route } from './+types/details'
 import type { Project } from '~/type'
+import { FaArrowLeft } from 'react-icons/fa'
+import { Link } from 'react-router'
 
 export async function clientLoader({ request, params }: Route.ClientLoaderArgs): Promise<Project> {
   const res = await fetch(`http://localhost:3000/projects/${params.id}`)
@@ -15,14 +17,36 @@ export function HydrateFallback() {
 }
 
 const ProjectDetails = ({ loaderData }: Route.ComponentProps) => {
-
   const project = loaderData
-  console.log(project)
 
   return (
     <>
       <div>
         <h2>Project Details</h2>
+        <Link to='/projects' className='flex items-center text-blue-400 hover:text-blue-500 mb-6 transition'>
+          <FaArrowLeft className='mr-2' /> Back to Projects
+        </Link>
+
+        <div className="grid gap-8 md:grid-cols-2 items-start">
+          <div>
+            <img src={project.image} alt={project.title} className='w-full rounded-lg shadow-md' />
+          </div>
+          <div>
+            <h1 className="text-3xl font-bold text-blue-400 mb-4">
+              {project.title}
+            </h1>
+            <p className="text-gray-300 text-sm mb-4">
+              {new Date(project.date).toLocaleDateString('default', { year: 'numeric', month: 'long', day: 'numeric' })} · {project.category}
+            </p>
+            <p className="text-gray-200 mb-6">
+              {project.description}
+            </p>
+
+            <a href={project.url} target="_blank" className='inline-block text-white bg-blue-600 hover:bg-blue-700 px-6 py-2 rounded transition hover:scale-110'>
+              View Live Site ➜
+            </a>
+          </div>
+        </div>
       </div>
     </>
   )
