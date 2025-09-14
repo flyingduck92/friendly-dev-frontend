2aa6544ddc75cbfb0e03f28f3691376ebb2ef562
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-09-14 15:13:14 +0700

    Add filter operation

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -12,24 +12,50 @@ export async function loader({ request }: Route.LoaderArgs): Promise<{ projects:
 }
 
 const Projects = ({ loaderData }: Route.ComponentProps) => {
-  const { projects } = loaderData as { projects: Project[] }
-
+  const [selectedCategory, setSelectedCategory] = useState('All')
   const [currentPage, setCurrentPage] = useState(1)
   const projectPerPage = 10
 
+  const { projects } = loaderData as { projects: Project[] }
+  // get unique categories
+  const categories = ['All', ...new Set(projects.map(project => project.category))]
+
+  // filter project based on category
+  const filteredProjects =
+    selectedCategory === 'All'
+      ? projects
+      : projects.filter(project => project.category === selectedCategory)
+
   // calculate totalPages
-  const totalPages = Math.ceil(projects.length / projectPerPage)
+  const totalPages = Math.ceil(filteredProjects.length / projectPerPage)
 
   // get current pages projects 
   const indexOfLast = currentPage * projectPerPage
   const indexOfFirst = indexOfLast - projectPerPage
-  const currentProjects = projects.slice(indexOfFirst, indexOfLast)
+  const currentProjects = filteredProjects.slice(indexOfFirst, indexOfLast)
 
   return (
     <>
       <h2 className="text-3xl font-bold text-white mb-8">
         🚀Projects
       </h2>
+
+      <div className="flex flex-wrap gap-2 mb-8">
+        {
+          categories.map(category => (
+            <button key={category}
+              className={`cursor-pointer px-3 py-1 rounded text-sm ${selectedCategory === category ? 'bg-blue-600 text-white' : 'bg-gray-700 text-gray-200'}`}
+              onClick={() => {
+                setSelectedCategory(category)
+                setCurrentPage(1)
+              }}
+            >
+              {category}
+            </button>
+          ))
+        }
+      </div>
+
       <div className="grid gap-6 sm:grid-cols-2">
         {
           currentProjects.map(project => (
@@ -37,6 +63,7 @@ const Projects = ({ loaderData }: Route.ComponentProps) => {
           ))
         }
       </div>
+
       <Pagination
         totalPages={totalPages}
         currentPage={currentPage}
