08e898e5661a6c330008353bca2c2f5dd82ff874
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-09-18 14:16:52 +0700

    Add filtered blog post component and add to blog index

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -4,6 +4,7 @@ import PostCard from '~/components/PostCard'
 
 import { useState } from 'react'
 import Pagination from '~/components/Pagination'
+import PostFilter from '~/components/PostFilter'
 
 export async function loader({ request }: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
   const url = new URL('/posts-meta.json', request.url)
@@ -20,24 +21,47 @@ export async function loader({ request }: Route.LoaderArgs): Promise<{ posts: Po
 }
 
 const Blog = ({ loaderData }: Route.ComponentProps) => {
+  const [searchQuery, setSearchQuery] = useState('')
   const [currentPage, setCurrentPage] = useState(1)
+
   const { posts } = loaderData
+  const filteredPosts = posts.filter(post => {
+    const query = searchQuery.toLowerCase()
+    return (
+      post.title.toLowerCase().includes(query)
+      || post.excerpt.toLowerCase().includes(query)
+    )
+  })
 
   const postPerPage = 10
-  const totalPages = Math.ceil(posts.length / postPerPage)
+  const totalPages = Math.ceil(filteredPosts.length / postPerPage)
   const indexOfLast = currentPage * postPerPage
   const indexOfFirst = indexOfLast - postPerPage
 
-  const currentPosts = posts.slice(indexOfFirst, indexOfLast)
+  const currentPosts = filteredPosts.slice(indexOfFirst, indexOfLast)
 
   return (
     <div className='max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900 '>
       <h2 className="text-3xl font-bold text-white mb-8">
         📰Blog
       </h2>
-      {currentPosts.map(post => (
-        <PostCard key={post.slug} post={post} />
-      ))}
+
+      <PostFilter
+        searchQuery={searchQuery}
+        onSearchChange={query => {
+          setSearchQuery(query)
+          setCurrentPage(1)
+        }} />
+
+      <div className="space-y-8">
+        {currentPosts.length < 1 ?
+          <p className="text-gray-400 text-center">No post found</p>
+          :
+          currentPosts.map(post => (
+            <PostCard key={post.slug} post={post} />
+          ))
+        }
+      </div>
 
       {
         totalPages > 1 &&
