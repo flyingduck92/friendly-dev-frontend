d30910959e5778f4c9e4b2a890d9ffa1bf369050
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-09-22 13:47:57 +0700

    Add formspree.io as contact form on contact page

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,7 +1,7 @@
 import type { Route } from './+types'
-import { Form } from 'react-router'
+// import { Form } from 'react-router'
 
-export async function action({ request }: Route.ActionArgs) {
+/* export async function action({ request }: Route.ActionArgs) {
   const formData = await request.formData()
   const name = formData.get('name') as string
   const email = formData.get('email') as string
@@ -27,10 +27,9 @@ export async function action({ request }: Route.ActionArgs) {
   // if no errors send data to backend/db
   const data = { name, email, subject, message }
   return { message: 'Form Submitted successfully', data }
-}
+} */
 
 const Contact = ({ actionData }: Route.ComponentProps) => {
-  const errors = actionData?.errors || {}
 
   return (
     <div className='max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900'>
@@ -38,69 +37,44 @@ const Contact = ({ actionData }: Route.ComponentProps) => {
         📬 Contact Me
       </h2>
 
-      {actionData?.message ? (
-        <p className='p-4 mb-6 bg-green-700 text-green-100 rounded-lg border border-green-500 shadow-md'>
-          {actionData.message}
-        </p>
-      ) : null}
-
-      <Form method='post' className="space-y-6">
+      <form action='https://formspree.io/f/xeorjzgq'
+        method='post' className="space-y-6">
         <div>
           <label htmlFor="name" className="block text-sm font-semibold text-gray-300">
             Full Name
           </label>
           <input type="text" id="name" name="name"
-            className={`w-full mt-1 px-4 py-2 border ${errors.name ? 'border-red-500' : 'border-gray-700'} rounded-lg bg-gray-800 text-gray-100`}
+            className={`w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100`}
           />
-          {errors.name && (
-            <p className="text-red-400 text-sm mt-1">
-              {errors.name}
-            </p>
-          )}
         </div>
         <div>
           <label htmlFor="email" className="block text-sm font-semibold text-gray-300">
             Email
           </label>
           <input type="email" id="email" name="email"
-            className={`w-full mt-1 px-4 py-2 border ${errors.email ? 'border-red-500' : 'border-gray-700'} rounded-lg bg-gray-800 text-gray-100`}
+            className={`w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100`}
           />
-          {errors.email && (
-            <p className="text-red-400 text-sm mt-1">
-              {errors.email}
-            </p>
-          )}
         </div>
         <div>
           <label htmlFor="subject" className="block text-sm font-semibold text-gray-300">
             Subject
           </label>
           <input type="text" id="subject" name="subject"
-            className={`w-full mt-1 px-4 py-2 border ${errors.subject ? 'border-red-500' : 'border-gray-700'} rounded-lg bg-gray-800 text-gray-100`}
+            className={`w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100`}
           />
-          {errors.subject && (
-            <p className="text-red-400 text-sm mt-1">
-              {errors.subject}
-            </p>
-          )}
         </div>
         <div>
           <label htmlFor="message" className="block text-sm font-semibold text-gray-300">
             Message
           </label>
           <textarea id="message" name="message"
-            className={`w-full mt-1 px-4 py-2 border ${errors.message ? 'border-red-500' : 'border-gray-700'} rounded-lg bg-gray-800 text-gray-100`}>
+            className={`w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100`}>
           </textarea>
-          {errors.message && (
-            <p className="text-red-400 text-sm mt-1">
-              {errors.message}
-            </p>
-          )}
         </div>
         <button className="cursor-pointer w-full bg-blue-600 hover:bg-blue-700 py-2 rounded-lg text-white">
           Send Message
         </button>
-      </Form>
+      </form>
     </div>
   )
 }
